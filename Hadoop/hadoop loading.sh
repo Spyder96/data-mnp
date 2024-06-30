6dfbf93001483619fb33a0e66d59ad176b205df8
Amirul's laptop <Amirul.1216@gmail.com>
2024-06-30 10:40:39 +0530

    hive loading

--- Hadoop/hadoop loading.sh
+++ Hadoop/hadoop loading.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# MySQL:Constants
+# Constants
 jdbcconnection="jdbc:mysql://localhost:3306/classicmodels"
 password="hadoop"
 username="root"
-targetfolder="/home/labuser/classicmodels/"
+targetfolder="/home/labuser/database/classicmodels/"
 mappingpartitions=2
 
 echo "Starting Hadoop and YARN services"
 
-start-all.sh
-mapred --daemon start historyserver
+#start-all.sh
+#mapred --daemon start historyserver
 
 
 # List tables
@@ -23,19 +23,33 @@ echo "Loading Tables to hdfs using sqoop"
 for table in $tablelist
 
 do
-  echo "Table name = $table , target dir = $targetfolder$table"
-  sqoop import -Dorg.apache.sqoop.splitter.allow_text_splitter=true --connect $jdbcconnection --password $password --username $username --table $table -m $mappingpartitions  --target-dir $targetfolder$table --delete-target-dir > /dev/null  
+  echo -e "****\n\nTable name = $table , target dir = $targetfolder$table\n\n****"
+  if [ "$table" = "productlines" ]; 
+    then 
+    sqoop import -Dorg.apache.sqoop.splitter.allow_text_splitter=true --connect $jdbcconnection --password $password --username $username --table $table -m $mappingpartitions  --target-dir $targetfolder$table --delete-target-dir --split-by textDescription > /dev/null  #condition for productlines failure resolve
+  else
+    sqoop import -Dorg.apache.sqoop.splitter.allow_text_splitter=true --connect $jdbcconnection --password $password --username $username --table $table -m $mappingpartitions  --target-dir $targetfolder$table --delete-target-dir > /dev/null  
+  fi
 
 echo "Import Complete"
 echo "****************************************"
 done
-### Failures productlines
+### Failures productlines fixed in if condition by changing column Set using
+### ALTER TABLE productlines MODIFY textDescription VARCHAR(4000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+### and then using split-by on the column
 
 
 for table in $tablelist;  
 do   
 mysql -h hdpserver -u hive -phadoop "classicmodels" -s -e "show create table $table;" | sed -n '1p' | cut -f2- | sed 's/\\n/\'$'\n''/g' |sed 's/ENGINE=InnoDB DEFAULT CHARSET=latin1//' | sed 's/TableCreate Table//'  > mysql_ddl_$table.sql
 grep -v "mysql: [Warning] Using a password on the command line interface can be insecure." mysql_ddl_$table.sql
+
+
+# Columns for Hive table creation
+columns=`mysql -h hdpserver -u hive -phadoop "classicmodels" --silent -e "SELECT column_name FROM information_schema.columns WHERE table_name='$table';" | grep -v "COLUMN_NAME" | sed ':a;N;$!ba;s/\n/, /g'`
+
+#Creating statements for Hive table
+echo  "CREATE TABLE $table ( $columns ) row format delimited fields terminated by "," stored as textfile location $targetfolder$table;" 
 
 
 done 
@@ -49,10 +63,12 @@ columns=`mysql -h hdpserver -u hive -phadoop "classicmodels" --silent -e "SELECT
 #Creating External Hive tables
 hive -e "DROP TABLE IF EXISTS $table"
 echo "Table name = $table"
-hive -e "CREATE EXTERNAL TABLE  $table ( $columns ) row format delimited fields terminated by ',' stored as textfile location '$targetfolder$table';"
+hive -e "CREATE TABLE  $table ( $columns ) row format delimited fields terminated by \",\" stored as textfile location \"$targetfolder$table\";"
 
 done
  #### Failures: orders, products, productlines
+
+## Fixing Failed list, fails are due to column type changes, this was done manually
 
 fail_list="
 orders
@@ -73,14 +89,12 @@ done
 
 ####
 hive -e "DROP  TABLE IF EXISTS orders;"
-hive -e "CREATE external TABLE  orders ( orderNumber int, orderDate date, requiredDate date, shippedDate date, status string, comments string, customerNumber int ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/classicmodels/ext/orders';"
+hive -e "CREATE external TABLE  orders ( orderNumber int, orderDate date, requiredDate date, shippedDate date, status string, comments string, customerNumber int ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/database/classicmodels/orders';"
 
 hive -e "DROP TABLE IF EXISTS products;"
-hive -e "CREATE TABLE  products ( productCode string, productName string, productLine string, productScale string, productVendor string, productDescription string, quantityInStock int, buyPrice decimal(10,2), MSRP decimal(10,2) ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/classicmodels/products';"
+hive -e "CREATE TABLE  products ( productCode string, productName string, productLine string, productScale string, productVendor string, productDescription string, quantityInStock int, buyPrice decimal(10,2), MSRP decimal(10,2) ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/database/classicmodels/products';"
 
 
 hive -e "DROP TABLE IF EXISTS productlines;"
-hive -e "CREATE TABLE  productlines ( productLine string, textDescription string, htmlDescription string, image string ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/classicmodels/productlines';"
-
-
+hive -e "CREATE TABLE  productlines ( productLine string, textDescription string, htmlDescription string, image string ) row format delimited fields terminated by ',' stored as textfile location '/home/labuser/database/classicmodels/productlines';"
 
